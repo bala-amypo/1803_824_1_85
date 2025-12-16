589e9dc899a2dbc3c46c7594e3344ff68fd91633
bala-amypo <bala@amypo.in>
2025-12-16 15:17:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/FacilityScore.java
+++ src/main/java/com/example/demo/entity/FacilityScore.java
@@ -2,5 +2,28 @@ package com.example.demo.entity;
 
 public class FacilityScore{
     private Long id;
-    private String
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+     
+     public Long getid(){
+        return id;
+     }
+     public void setid(Long id){
+        this.id=id;
+     }
+     public String getname(){
+        return name;
+     }
+     public void setname(){
+        this.name=name;
+     }
+     public String getemail(){
+        return email;
+     }
+     public void setemail(){
+        this.name=name;
+     }
+
 }
