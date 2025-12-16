b6ba391620156e99a22d675331d57d0a59bcf733
bala-amypo <bala@amypo.in>
2025-12-16 15:22:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Property.java
@@ -0,0 +1,42 @@
+package com.example.demo.entity;
+
+public class Property{
+    private Long id;
+    private String title;
+    private String address;
+    private String city;
+    private Double price;
+    private Double  areaSqFt;
+     
+     public Long getid(){
+        return id;
+     }
+     public void setid(Long id){
+        this.id=id;
+     }
+     public String gettitle(){
+        return title;
+     }
+     public void settitle(){
+        this.title=title;
+     }
+     public String getaddress(){
+        return address;
+     }
+     public void setemail(){
+        this.email=email;
+     }
+     public String getpassword(){
+        return password;
+     }
+     public void setpassword(){
+        this.password=password;
+     }
+     public String getrole(){
+        return role;
+     }
+     public void setrole(){
+        this.role=role;
+     }
+
+}
