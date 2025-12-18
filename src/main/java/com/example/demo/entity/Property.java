dcdd4fc0261a1290fd5f8b41b61147f29f08d20d
bala-amypo <bala@amypo.in>
2025-12-18 03:52:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/Property.java
+++ src/main/java/com/example/demo/entity/Property.java
@@ -1,70 +1,70 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
-// @Entity
-// public class Property{
+@Entity
+public class Property{
 
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     private String title;
-//     private String address;
-//     private String city;
-//     private Double price;
-//     private Double  areaSqFt;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String address;
+    private String city;
+    private Double price;
+    private Double  areaSqFt;
      
-//      public Long getid(){
-//         return id;
-//      }
-//      public void setid(Long id){
-//         this.id=id;
-//      }
-//      public String gettitle(){
-//         return title;
-//      }
-//      public void settitle(String title){
-//         this.title=title;
-//      }
-//      public String getaddress(){
-//         return address;
-//      }
-//      public void setaddress(String address){
-//         this.address=address;
-//      }
-//      public String getcity(){
-//         return city;
-//      }
-//      public void setcity(String city){
-//         this.city=city;
-//      }
-//      public Double getprice(){
-//         return price;
-//      }
-//      public void setprice(Double price){
-//         this.price=price;
-//      }
-//      public Double getareaSqFt(){
-//         return areaSqFt;
-//      }
-//      public void setareaSqFt(Double areaSqFt){
-//         this.areaSqFt=areaSqFt;
-//      }
-//     public Property(Long id,String title, String address,String city,Double price, Double areaSqFt){
-//       this.id=id;
-//       this.title=title;
-//       this.address=address;
-//       this.city=city;
-//       this.price=price;
-//       this.areaSqFt=areaSqFt;
+     public Long getid(){
+        return id;
+     }
+     public void setid(Long id){
+        this.id=id;
+     }
+     public String gettitle(){
+        return title;
+     }
+     public void settitle(String title){
+        this.title=title;
+     }
+     public String getaddress(){
+        return address;
+     }
+     public void setaddress(String address){
+        this.address=address;
+     }
+     public String getcity(){
+        return city;
+     }
+     public void setcity(String city){
+        this.city=city;
+     }
+     public Double getprice(){
+        return price;
+     }
+     public void setprice(Double price){
+        this.price=price;
+     }
+     public Double getareaSqFt(){
+        return areaSqFt;
+     }
+     public void setareaSqFt(Double areaSqFt){
+        this.areaSqFt=areaSqFt;
+     }
+    public Property(Long id,String title, String address,String city,Double price, Double areaSqFt){
+      this.id=id;
+      this.title=title;
+      this.address=address;
+      this.city=city;
+      this.price=price;
+      this.areaSqFt=areaSqFt;
 
 
-//    }
-//    public Property(){
+   }
+   public Property(){
       
-//    }
+   }
 
-// }
+}
--- src/main/java/com/example/demo/entity/RatingLog.java
+++ src/main/java/com/example/demo/entity/RatingLog.java
@@ -1,55 +1,55 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
 
-// @Entity
-// public class RatingLog{
-//     @Id
-//     @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private Long id;
-//     private Property property;
-//     private String message;
-//     private LocalDateTime loggedAt;
+@Entity
+public class RatingLog{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private Property property;
+    private String message;
+    private LocalDateTime loggedAt;
     
      
-//      public Long getid(){
-//         return id;
-//      }
-//      public void setid(Long id){
-//         this.id=id;
-//      }
-//      public Property setproperty(){
-//         return property;
-//      }
-//      public void getproperty(Property property){
-//         this.property=property;
-//      }
-//      public String getmessage(){
-//         return message;
-//      }
-//      public void setmessage(String message){
-//         this.message=message;
-//      }
+     public Long getid(){
+        return id;
+     }
+     public void setid(Long id){
+        this.id=id;
+     }
+     public Property setproperty(){
+        return property;
+     }
+     public void getproperty(Property property){
+        this.property=property;
+     }
+     public String getmessage(){
+        return message;
+     }
+     public void setmessage(String message){
+        this.message=message;
+     }
      
-//      public LocalDateTime getloggedAt(){
-//         return loggedAt;
-//      }
-//      public void setloggedAt(LocalDateTime loggedAt){
-//         this.loggedAt=loggedAt;
-//      }
+     public LocalDateTime getloggedAt(){
+        return loggedAt;
+     }
+     public void setloggedAt(LocalDateTime loggedAt){
+        this.loggedAt=loggedAt;
+     }
 
-//    public RatingLog(Long id,Property property,String message,LocalDateTime loggedAt){
-//       this.id=id;
-//       this.property=property;
-//       this.message=message;
-//       this.loggedAt=loggedAt;
-//    }
-//    public RatingLog(){
+   public RatingLog(Long id,Property property,String message,LocalDateTime loggedAt){
+      this.id=id;
+      this.property=property;
+      this.message=message;
+      this.loggedAt=loggedAt;
+   }
+   public RatingLog(){
       
-//    }
+   }
      
-// }
+}
