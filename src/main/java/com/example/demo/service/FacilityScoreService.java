401d5a70837ee8e34604fc397b73507667ada098
bala-amypo <bala@amypo.in>
2025-12-18 09:47:34 +0000

    User commit

--- src/main/java/com/example/demo/service/FacilityScoreService.java
+++ src/main/java/com/example/demo/service/FacilityScoreService.java
@@ -2,6 +2,6 @@ package com.example.demo.service;
 
 import com.example.demo.entity.FacilityScore;
 public interface FacilityScoreService{
-    void  FacilityScore addScore(Long propertyId, FacilityScore score);
-    int  FacilityScore getScoreByProperty(Long propertyId);
+    FacilityScore addScore(Long propertyId, FacilityScore score);
+    FacilityScore getScoreByProperty(Long propertyId);
 }
