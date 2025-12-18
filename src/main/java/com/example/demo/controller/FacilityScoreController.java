9105f9c353f88c6585c73c03210c4cb430a75a98
bala-amypo <bala@amypo.in>
2025-12-18 08:43:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityScoreController.java
+++ src/main/java/com/example/demo/controller/FacilityScoreController.java
@@ -4,24 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.entity.FacilityScore;
-import com.example.demo.service.FacilityService;
+
+import com.example.demo.entity.AuditTrailRecord;
+import com.example.demo.service.AuditTrailRecordService;
 
 @RestController
 public class FacilityScoreController {
     @Autowired
     FacilityService fs;
-    
 
-    @PostMapping("/register")
-    public FacilityScore addfacility(@RequestBody User fss){
-        return fs.addScore(fss);
+    @PostMapping("/AuditTrail")
+    public AuditTrailRecord addAuditTrailRecord(@RequestBody AuditTrailRecord atr){
+        return atrs.logEvent(atr);
     }
 
-    @PostMapping("/login")
-    public FacilityScore addsfacility(@RequestBody User uss){
-        return fs.getScore(fss);
+    @GetMapping("/")
+    public int first(Long credentialId){
+        return atrs.getLogs();
     }
 
-
+    @GetMapping
+    public List<AuditTrailRecord> second(){
+        return atrs.getAllLogs();
+    }
 }
