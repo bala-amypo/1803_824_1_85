616e147b260b1aafadcd2245789b012c2def3522
bala-amypo <bala@amypo.in>
2025-12-18 15:29:10 +0000

    User commit

--- src/main/java/com/example/demo/service/impls/Propertyimpls.java
+++ src/main/java/com/example/demo/service/impls/Propertyimpls.java
@@ -1,2 +1,26 @@
 package com.example.demo.controller;
-import java.util
+
+import java.util.List;
+import org.Springframwork.beans.factory.annotation.Autowired;
+import org.Springframwork.stereotype.Service;
+
+import com.example.demo.entity.Property;
+import com.example.demo.repository.PropertyRepository;
+import com.example.demo.service.PropertyService;
+
+@Service
+public class PropertyServiceImpl implements PropertyService {
+
+    @Autowired
+    private PropertyRepository repo;
+
+    @Override
+    public Property addProperty(Property property) {
+        return repo.save(property);
+    }
+
+    @Override
+    public List<Property> getAllProperties() {
+        return repo.findAll();
+    }
+}
