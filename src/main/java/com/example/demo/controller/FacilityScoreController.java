6957fe3783d657b0e4df2f9fa793ca76f1eb62b2
bala-amypo <bala@amypo.in>
2025-12-18 09:37:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityScoreController.java
+++ src/main/java/com/example/demo/controller/FacilityScoreController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springFramework.web.bind.annotation.Ge
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
