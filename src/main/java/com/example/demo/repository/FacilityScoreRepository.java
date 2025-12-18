f0f900472c90a72f0443d87469047b2fca7cd0e7
bala-amypo <bala@amypo.in>
2025-12-18 06:48:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/FacilityScoreRepository.java
+++ src/main/java/com/example/demo/repository/FacilityScoreRepository.java
@@ -5,7 +5,7 @@
 
     import com.example.demo.entity.FacilityScore;
 
-    import java.util.
+    import java.util.Long;
 
     @Repository
     public interface FacilityScoreRepository extends JpaRepository<FacilityScore, Long> {
--- src/main/java/com/example/demo/service/FacilityScoreService.java
+++ src/main/java/com/example/demo/service/FacilityScoreService.java
@@ -2,6 +2,6 @@ package com.example.demo.service;
 
 import com.example.demo.entity.FacilityScore;
 public interface FacilityScoreService{
-    FacilityScore addScore(Long propertyId, FacilityScore score);
-    FacilityScore getScoreByProperty(Long propertyId);
+    void  FacilityScore addScore(Long propertyId, FacilityScore score);
+    int  FacilityScore getScoreByProperty(Long propertyId);
 }
