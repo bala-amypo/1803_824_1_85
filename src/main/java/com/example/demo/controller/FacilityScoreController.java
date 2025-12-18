53d61bb3fe638ad8bd27bf716d1934bd75af5dca
bala-amypo <bala@amypo.in>
2025-12-18 09:12:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityScoreController.java
+++ src/main/java/com/example/demo/controller/FacilityScoreController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.AuditTrailRecord;
-import com.example.demo.service.Service;
+import com.example.demo.entity.FacilityScore;
+import com.example.demo.service.FacilityScoreService;
 
 @RestController
 public class FacilityScoreController {
