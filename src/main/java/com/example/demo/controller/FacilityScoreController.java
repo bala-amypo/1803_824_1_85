1e8131df809ed97989050dbc953d76ddd04581f3
bala-amypo <bala@amypo.in>
2025-12-18 13:25:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityScoreController.java
+++ src/main/java/com/example/demo/controller/FacilityScoreController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.service.StudentService;
+import com.example.demo.entity.FacilityScore;
+import com.example.demo.service.FacilityService;
 
 
 @RestController
@@ -21,7 +21,7 @@ public class FacilityScoreController {
     FacilityScoreService fs;
 
     @PostMapping("/{propertyId}")
-    public FacilityScore addScore(@RequestBody FacilityScore student){
+    public FacilityScore addScore(@RequestBody FacilityScore  ){
         return fs.addScore();
     }
     
