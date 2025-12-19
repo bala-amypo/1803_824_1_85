ee4e881509c9f553c23e60a2567a5bca975c8065
bala-amypo <bala@amypo.in>
2025-12-19 09:12:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,15 +1,15 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.example.demo.entity.User;
-// import com.example.demo.service.UserService;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
 // @RestController
 // @RequestMapping("/auth")
@@ -48,7 +48,7 @@ public class AuthController {
         }
 
         User savedUser = service.register(user);
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED); // Return saved user with 201 status
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED); 
     }
 
     
@@ -59,4 +59,6 @@ public class AuthController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND); 
         }
         return new ResponseEntity<>(existingUser, HttpStatus.OK); 
+}
+
 }
