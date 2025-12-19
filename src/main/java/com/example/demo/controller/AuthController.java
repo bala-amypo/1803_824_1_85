89474ca6f7bec72ba6c8f06228c55d1670774d2d
bala-amypo <bala@amypo.in>
2025-12-19 16:04:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -25,7 +25,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public User login(@RequestBody User user) {
-        return service.login(user.getemail());
+        return service.findByemail(user.getemail());
     }
 
     
