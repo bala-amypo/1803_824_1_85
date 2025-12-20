b4fa65af6750e52de21a9e019df25be5594f95cf
bala-amypo <bala@amypo.in>
2025-12-20 05:03:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/FacilityScore.java
+++ src/main/java/com/example/demo/entity/FacilityScore.java
@@ -4,57 +4,60 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManytoOne;
 
 @Entity
 public class FacilityScore{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    @ManytoOne
+    private Property property;
     private Integer schoolProximity;
     private Integer hospitalProximity;
     private Integer transportAccess;
     private Integer safetyScore;
    
      
-   public Long getid(){
+   public Long getId(){
         return id;
      }
-   public void setid(Long id){
+   public void setId(Long id){
         this.id=id;
      }
 
-   public Integer setschoolProximity(){
+   public Integer setSchoolProximity(){
         return schoolProximity;
      }
-   public void getschoolProximity(Integer schoolProximity){
+   public void getSchoolProximity(Integer schoolProximity){
         this.schoolProximity=schoolProximity;
      }
 
-   public Integer sethospitalProximity(){
+   public Integer setHospitalProximity(){
         return hospitalProximity;
      }
 
-   public void gethospitalProximity(Integer hospitalProximity){
+   public void getHospitalProximity(Integer hospitalProximity){
         this.hospitalProximity=hospitalProximity;
      }
 
-   public Integer settransportAccess(){
+   public Integer setTransportAccess(){
         return transportAccess;
      }
-   public void gettransportAccess(Integer transportAccess){
+   public void getTransportAccess(Integer transportAccess){
         this.transportAccess=transportAccess;
      }
 
-   public Integer setsafetyScore(){
+   public Integer setSafetyScore(){
         return safetyScore;
      }
-   public void getsafetyScore(Integer safetyScore){
+   public void getSafetyScore(Integer safetyScore){
         this.safetyScore=safetyScore;
      }
 
-   public FacilityScore(Long id, Integer schoolProximity,Integer hospitalProximity,Integer transportAccess,Integer safetyScore){
+   public FacilityScore(Long id,Pr Integer schoolProximity,Integer hospitalProximity,Integer transportAccess,Integer safetyScore){
       this.id=id;
-      
+      this.property=property;
       this.schoolProximity=schoolProximity;
       this.hospitalProximity=hospitalProximity;
       this.transportAccess=transportAccess;
