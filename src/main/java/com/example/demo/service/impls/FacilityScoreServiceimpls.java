a51ecfae671c167bb24a14f139a56abf91c78b1f
bala-amypo <bala@amypo.in>
2025-12-20 05:12:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impls/FacilityScoreServiceimpls.java
@@ -0,0 +1,47 @@
+package com.example.demo.service.impls;
+
+import com.example.demo.entity.FacilityScore;
+import com.example.demo.entity.Property;
+import com.example.demo.repository.FacilityScoreRepository;
+import com.example.demo.repository.PropertyRepository;
+import com.example.demo.service.FacilityScoreService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class FacilityScoreServiceimpls implements FacilityScoreService {
+
+    private final FacilityScoreRepository facilityScoreRepository;
+    private final PropertyRepository propertyRepository;
+
+    public FacilityScoreServiceImpl(FacilityScoreRepository facilityScoreRepository,
+                                    PropertyRepository propertyRepository) {
+        this.facilityScoreRepository = facilityScoreRepository;
+        this.propertyRepository = propertyRepository;
+    }
+
+    @Override
+    public FacilityScore addScore(Long propertyId, FacilityScore score) {
+        Property property = propertyRepository.findById(propertyId)
+                .orElseThrow(() -> new RuntimeException("Property not found"));
+
+        
+        FacilityScore existing = facilityScoreRepository.findByPropertyId(propertyId);
+        if (existing != null) {
+            throw new RuntimeException("Facility score already exists for this property");
+        }
+
+        score.setProperty(property);
+        return facilityScoreRepository.save(score);
+    }
+
+    @Override
+    public FacilityScore getScoreByProperty(Long propertyId) {
+        return facilityScoreRepository.findByPropertyId(propertyId);
+    }
+
+    @Override
+    public List<FacilityScore> getAllScoresByProperty(Long propertyId) {
+        return facilityScoreRepository.findAllByPropertyId(propertyId);
+    }
+}
