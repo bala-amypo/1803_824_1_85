fb685e784571ab821d811bff31f9e3dc35840eb2
bala-amypo <bala@amypo.in>
2025-12-20 04:31:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impls/FacilityScoreimpls.java
@@ -0,0 +1,27 @@
+package com.example.demo.service.impls;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Property;
+import com.example.demo.repository.PropertyRepository;
+import com.example.demo.service.PropertyService;
+
+@Service
+public class PropertyServiceimpls implements PropertyService {
+
+    @Autowired
+    private PropertyRepository repo;
+
+    @Override
+    publicFacilityScore addProperty(Property property) {
+        return repo.save(property);
+    }
+
+    @Override
+    public List<FacilityScore> getAllProperties() {
+        return repo.findAll();
+    }
+}
