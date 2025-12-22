37c292bdd14991466dc210e8e353c02f7258c355
bala-amypo <bala@amypo.in>
2025-12-22 00:56:16 +0000

    User commit

--- src/main/java/com/example/demo/service/FacilityScoreService.java
+++ src/main/java/com/example/demo/service/FacilityScoreService.java
@@ -1,33 +1,4 @@
 
-// package com.example.demo.service;
-
-// import com.example.demo.entity.FacilityScore;
-// import java.util.List;
-
-// public interface FacilityScoreService {
-
-//     FacilityScore addScore(Long propertyId, FacilityScore score);
-
-//     FacilityScore getScoreByProperty(Long propertyId);
-
-//     List<FacilityScore> getAllScoresByProperty(Long propertyId);
-// }
-
-// package com.example.demo.service;
-
-// import com.example.demo.entity.FacilityScore;
-// import java.util.List;
-
-// public interface FacilityScoreService {
-
-//     FacilityScore addScore(Long propertyId, FacilityScore score);
-
-//     FacilityScore getScoreByProperty(Long propertyId);
-
-//     List<FacilityScore> getAllScoresByProperty(Long propertyId);
-// }
-
-
 package com.example.demo.service;
 
 import com.example.demo.entity.FacilityScore;
--- src/main/java/com/example/demo/service/impls/PropertyServiceimpls.java
+++ src/main/java/com/example/demo/service/impls/PropertyServiceimpls.java
@@ -1,30 +1,4 @@
-// package com.example.demo.service.impls;
 
-// import java.util.List;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import com.example.demo.entity.Property;
-// import com.example.demo.repository.PropertyRepository;
-// import com.example.demo.service.PropertyService;
-
-// @Service
-// public class PropertyServiceimpls implements PropertyService {
-
-//     @Autowired
-//     private PropertyRepository repo;
-
-//     @Override
-//     public Property addProperty(Property property) {
-//         return repo.save(property);
-//     }
-
-//     @Override
-//     public List<Property> getAllProperties() {
-//         return repo.findAll();
-//     }
-//}
 
 package com.example.demo.service.impls;
 
