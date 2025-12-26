93fe5496b398b64a7c9230b29bb9f9dd0691b6b5
bala-amypo <bala@amypo.in>
2025-12-26 14:41:00 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -134,40 +134,42 @@
 // }
 
 
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import com.example.demo.security.JwtAuthenticationFilter;
-import org.springframework.context.annotation.*;
-import org.springframework.security.authentication.*;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.*;
-import org.springframework.security.web.SecurityFilterChain;
+// import com.example.demo.security.JwtAuthenticationFilter;
+// import org.springframework.context.annotation.*;
+// import org.springframework.security.authentication.*;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.crypto.bcrypt.*;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfig {
+// @Configuration
+// public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//     @Bean
+//     public PasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
+//     @Bean
+//     public AuthenticationManager authenticationManager(
+//             AuthenticationConfiguration config) throws Exception {
+//         return config.getAuthenticationManager();
+//     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//     @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**", "/swagger-ui/**").permitAll()
-                .requestMatchers("/properties/**").authenticated()
-                .anyRequest().authenticated()
-            );
+//         http.csrf(csrf -> csrf.disable())
+//             .authorizeHttpRequests(auth -> auth
+//                 .requestMatchers("/auth/**", "/swagger-ui/**").permitAll()
+//                 .requestMatchers("/properties/**").authenticated()
+//                 .anyRequest().authenticated()
+//             );
 
-        return http.build();
-    }
-}
+//         return http.build();
+//     }
+// }
+
+
 
