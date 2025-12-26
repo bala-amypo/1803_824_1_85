add009011cfdc8610c775a0207f374afc4aa0b3f
bala-amypo <bala@amypo.in>
2025-12-26 09:48:38 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiError.java
+++ src/main/java/com/example/demo/exception/ApiError.java
@@ -17,8 +17,35 @@
 
 
 
-package com.example.demo.exception;
+// package com.example.demo.exception;
 
+// import java.time.LocalDateTime;
+
+// public class ApiError {
+//     private LocalDateTime timestamp;
+//     private int status;
+//     private String error;
+//     private String message;
+//     private String path;
+
+//     public ApiError() {}
+//     public ApiError(LocalDateTime timestamp, int status, String error, String message, String path) {
+//         this.timestamp = timestamp;
+//         this.status = status;
+//         this.error = error;
+//         this.message = message;
+//         this.path = path;
+//     }
+//     // Getters
+//     public LocalDateTime getTimestamp() { return timestamp; }
+//     public int getStatus() { return status; }
+//     public String getError() { return error; }
+//     public String getMessage() { return message; }
+//     public String getPath() { return path; }
+//}
+
+
+package com.example.demo.exception;
 import java.time.LocalDateTime;
 
 public class ApiError {
@@ -28,7 +55,6 @@ public class ApiError {
     private String message;
     private String path;
 
-    public ApiError() {}
     public ApiError(LocalDateTime timestamp, int status, String error, String message, String path) {
         this.timestamp = timestamp;
         this.status = status;
