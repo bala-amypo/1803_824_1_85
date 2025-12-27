75d810e8645ea258f796c99b59f58c02f9c753d7
bala-amypo <bala@amypo.in>
2025-12-27 03:30:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/PropertyController.java
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -1,34 +1,4 @@
-// package com.example.demo.controller;
 
-// import java.util.List;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.demo.entity.Property;
-// import com.example.demo.service.PropertyService;
-
-// @RestController
-// @RequestMapping("/properties")
-
-// public class PropertyController {
-
-//     @Autowired
-//     private PropertyService service;
-
-//     @PostMapping
-//     public Property addProperty(@RequestBody Property property) {
-//         return service.addProperty(property);
-//     }
-
-//     @GetMapping
-//     public List<Property> getAll() {
-//         return service.getAllProperties();
-//     }
-//}
 
 package com.example.demo.controller;
 
@@ -74,30 +44,3 @@ public class PropertyController {
 }
 
 
-// package com.example.demo.controller;
-
-// import com.example.demo.entity.Property;
-// import com.example.demo.service.PropertyService;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.prepost.PreAuthorize;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/properties")
-// public class PropertyController {
-//     private final PropertyService propertyService;
-//     public PropertyController(PropertyService ps) { this.propertyService = ps; }
-
-//     @PostMapping
-//     // Restrict creation to ADMIN to pass testAddPropertyForbiddenForAnalyst
-//     public ResponseEntity<Property> addProperty(@RequestBody Property property) {
-//         return new ResponseEntity<>(propertyService.addProperty(property), HttpStatus.CREATED);
-//     }
-
-//     @GetMapping
-//     public ResponseEntity<List<Property>> getAllProperties() {
-//         return ResponseEntity.ok(propertyService.getAllProperties());
-//     }
-// }
